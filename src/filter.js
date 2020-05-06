d2f3dac6c3f93cd2f5341d05bc794e875bea2f44
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-05 20:49:35 -0500

    I got it

--- src/filter.js
+++ src/filter.js
@@ -1,6 +1,16 @@
 const makeFilter = (date) => {
-  let filters = [];
-  return filters;
+  // let filters = [week, month, ninety, year, years,quater ];
+  const currentTime = date.getTime();
+  const lastWeek = currentTime-(1000*60*60*24*7);
+  console.log(`the current time is : ${currentTime}\nAnd the last week was: ${lastWeek}`);
+  
+  const d = new Date(0);
+  const startAt = d.setUTCMilliseconds(lastWeek)
+  console.log(new Date(startAt));
+  
+  // return filters;
 }
 
-module.exports = { makeFilter };
+makeFilter(new Date(2020,0,1))
+
+// module.exports = { makeFilter };
