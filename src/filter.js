9471fead13f6fde8bab572b1ca7670ee6f54c1a3
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-06 20:18:11 -0500

    got problem importing and using require

--- src/filter.js
+++ src/filter.js
@@ -1,94 +1,77 @@
-import { lastDayOfMonth } from "date-fns";
+import fns from 'date-fns'
+const { format , sub, startOfYear, endOfYear, startOfMonth, endOfMonth, locale} = fns;
 
 const makeFilter = (date) => {
-  const entryDate = date.getTime();
-  const DAY = 1000*60*60*24
-  const last7daysRange = entryDate-(DAY*7);
-  const last28daysRange = entryDate-(DAY*28);
-  const last90daysRange = entryDate-(DAY*90);
-  const last365daysRange = entryDate-(DAY*365);
-  const last3yearsRanges = [last365daysRange,last365daysRange-(DAY*365),last365daysRange-(DAY*365*2)];
-
+  const expetedFormat =  'yyyy/MM/dd'
+  const last7daysRange = format(sub(date,{days: 7}), expetedFormat);
+  const last28daysRange =  format(sub(date,{days: 28}), expetedFormat);
+  const last90daysRange =  format(sub(date,{days: 90}), expetedFormat);
+  const last365daysRange =  format(sub(date,{days: 365}), expetedFormat);
   
-  console.log(`the current time is : ${entryDate}\nAnd the last week was: ${last7daysRange}`);
   
-  const threater = (range) =>{
-    const d = new Date(0);
-    const milisecondsTime = d.setUTCMilliseconds(range);
-    return new Date(milisecondsTime)
-  }
-
+  
   const last7days = {
     label: 'Últimos 7 días',
-    startAt: threater(last7daysRange).toLocaleDateString('en-ZA'),
-    endAt: date.toLocaleDateString('en-ZA')
+    startAt: last7daysRange,
+    endAt: format(date,expetedFormat)
   };
-
+  
   const last28days = {
     label: 'Últimos 28 días',
-    startAt: threater(last28daysRange).toLocaleDateString('en-ZA'),
-    endAt: date.toLocaleDateString('en-ZA')
+    startAt: last28daysRange,
+    endAt: format(date,expetedFormat)
   }
   
   const last90days = {
-    label: 'Últimos 28 días',
-    startAt: threater(last90daysRange).toLocaleDateString('en-ZA'),
-    endAt: date.toLocaleDateString('en-ZA')
+    label: 'Últimos 90 días',
+    startAt: last90daysRange,
+    endAt: format(date,expetedFormat)
   }
-
+  
   const last365days = {
     label: 'Últimos 365 días',
-    startAt: threater(last365daysRange).toLocaleDateString('en-ZA'),
-    endAt: date.toLocaleDateString('en-ZA')
+    startAt: last365daysRange,
+    endAt: format(date,expetedFormat)
   }
+  
+  const datesGenerator = (date) =>{
+    const arr = [];
+    for (let i=1 ; i <= 3; i++){
+      const a = sub(date,{years: i});
+      const y = {
+        label: format(a,'yyyy'),
+        startAt: format(startOfYear(a), expetedFormat),
+        endAt: format (endOfYear(new Date(a)), expetedFormat)
+      }
+      const b = sub(date,{months: i});
+      const z = {
+        label: format(b,'MMMM'),
+        startAt: format (startOfYear(b), expetedFormat),
+        endAt: format (endOfYear(new Date(b)), expetedFormat)
+      }
+      
+      arr.push(z)
+      console.log(`fecha añadida ${z.label} ${z.startAt} ${z.endAt}`);      
+    }
+    
+    
+    return arr;
+  }
+  
+  const arr = datesGenerator(date);
+  
+  const last3years = arr;
 
-  const last3years = [
-    {
-      label: threater(last3yearsRanges[0]).getFullYear().toString(),
-      startAt: threater(last3yearsRanges[0]).toLocaleDateString('en-ZA'),
-      endAt: threater(last3yearsRanges[0]+(DAY*364)).toLocaleDateString('en-ZA')
-    },
-    {
-      label: threater(last3yearsRanges[1]).getFullYear().toString(),
-      startAt: threater(last3yearsRanges[1]).toLocaleDateString('en-ZA'),
-      endAt: threater(last3yearsRanges[1]+(DAY*364)).toLocaleDateString('en-ZA')
-    },
-    {
-      label: threater(last3yearsRanges[2]).getFullYear().toString(),
-      startAt: threater(last3yearsRanges[2]).toLocaleDateString('en-ZA'),
-      endAt: threater(last3yearsRanges[2]+(DAY*364)).toLocaleDateString('en-ZA')
-    },
-  ]
-
-  const lastMonth = date.getMonth() - 1;
-  console.log(lastMonth);
-   
-
-  const lastquarter = [
-    {
-      label: date.toLocaleDateString('default', { month: 'long'}),
-      startAt: threater(last3yearsRanges[0]).toLocaleDateString('en-ZA'),
-      endAt: threater(last3yearsRanges[0]+(DAY*364)).toLocaleDateString('en-ZA')
-    },
-    {
-      label: threater(last3yearsRanges[1]).getFullYear().toString(),
-      startAt: threater(last3yearsRanges[1]).toLocaleDateString('en-ZA'),
-      endAt: threater(last3yearsRanges[1]+(DAY*364)).toLocaleDateString('en-ZA')
-    },
-    {
-      label: threater(last3yearsRanges[2]).getFullYear().toString(),
-      startAt: threater(last3yearsRanges[2]).toLocaleDateString('en-ZA'),
-      endAt: threater(last3yearsRanges[2]+(DAY*364)).toLocaleDateString('en-ZA')
-    },
-  ]
-  let filters = [last7days, last28days, last90days, last365days, last3years, lastquarter
-    // ,quater 
-  ];
-
-  console.log(filters);
-  // return filters;
+          let filters = [last7days, last28days, last90days, last365days, last3years,
+            //    lastquarter
+            // ,quater 
+          ];
+          
+          console.log(filters);
+          console.log(format(date,'MMMM', locale));
+          // return filters;
 }
-
-makeFilter(new Date(2020,0,1))
-
-// module.exports = { makeFilter };
+        
+        makeFilter(new Date(2020,0,1))
+        
+        // module.exports = { makeFilter };
